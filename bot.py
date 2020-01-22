1126786084402363637c2b83d3c637a27eb1425f
billpulec <bill.pulec@gmail.com>
2020-01-22 15:18:38 -0600

    Fixed error where bot wouldn't respond to replys in thread

--- bot.py
+++ bot.py
@@ -29,8 +29,8 @@ def check_mentions(api, since_id):
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
-        if tweet.in_reply_to_status_id is not None:
-            continue
+        #if tweet.in_reply_to_status_id is not None:
+        #    continue
 
         logger.info("Answering to " + tweet.user.screen_name)
 
@@ -60,7 +60,7 @@ def get_image(api, url):
 
 def main():
     api = create_api()
-    since_id = 1
+    since_id = 19
     while True:
         since_id = check_mentions(api, since_id)
         logger.info("Waiting...")
